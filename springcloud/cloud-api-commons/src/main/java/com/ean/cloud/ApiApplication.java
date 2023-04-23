5b9e98487e4de9109d0ba61014a60ac813b1d44c
Povlean <1927079760@qq.com>
2023-04-23 19:09:02 +0800

    "third commit"

--- springcloud/cloud-api-commons/src/main/java/com/ean/cloud/ApiApplication.java
+++ springcloud/cloud-api-commons/src/main/java/com/ean/cloud/ApiApplication.java
@@ -1,18 +1,15 @@
 package com.ean.cloud;
 
-import com.sun.glass.ui.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description:TODO
  * @author:Povlean
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+public class ApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(ApiApplication.class);
     }
 }
